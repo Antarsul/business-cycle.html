a24c44aecc242fa44f1d3d5de002f806e9c6921f
Antarsul <sulaijmanantar1995@gmail.com>
2026-04-18 18:34:12 -0400

    Update fmp.js

--- api/fmp.js
+++ api/fmp.js
@@ -2,14 +2,14 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-  const { ticker, timeseries = 252 } = req.query;
-
+  const { ticker } = req.query;
   if (!ticker) {
     return res.status(400).json({ error: 'Missing ticker' });
   }
 
   const API_KEY = 'dz3Lx2iSE0OcuZmpDfJF8bUVLPF8wOwi';
-  const url = `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}?timeseries=${timeseries}&apikey=${API_KEY}`;
+  // NEW ENDPOINT
+  const url = `https://financialmodelingprep.com/stable/historical-price-eod/full?symbol=${ticker}&apikey=${API_KEY}`;
 
   try {
     const response = await fetch(url);
